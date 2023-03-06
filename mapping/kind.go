81fd4b48e11d2c08a71b61e3fd2025ef92b34393
wanghao <wanghao@casachina.com.cn>
2023-03-06 14:21:12 +0800

    add mapping pkg

---
+++ mapping/kind.go
@@ -0,0 +1,12 @@
+package mapping
+
+const (
+	UINT8Size  = 1
+	INT8Size   = 1
+	UINT16Size = UINT8Size * 2
+	INT16Size  = INT8Size * 2
+	UINT32Size = UINT16Size * 2
+	INT32Size  = INT16Size * 2
+	UINT64Size = UINT32Size * 2
+	INT64Size  = INT32Size * 2
+)
