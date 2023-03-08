61f8df177fc193961165bccbb08c4265f7334604
wanghao <wanghao@casachina.com.cn>
2023-03-08 15:19:57 +0800

    upd mapping pkg

--- mapping/kind.go
+++ mapping/kind.go
@@ -1,5 +1,7 @@
 package mapping
 
+import "encoding/binary"
+
 const (
 	UINT8Size  = 1
 	INT8Size   = 1
@@ -10,3 +12,46 @@ const (
 	UINT64Size = UINT32Size * 2
 	INT64Size  = INT32Size * 2
 )
+
+type BytesToNumFuncType func(bytes []byte) int
+type NumToBytesFuncType func(num int, bytes []byte)
+
+var (
+	bytesToNumFuncMap = map[int]BytesToNumFuncType{
+		UINT8Size: func(bytes []byte) int {
+			return int(bytes[0])
+		},
+		UINT16Size: func(bytes []byte) int {
+			return int(binary.BigEndian.Uint16(bytes))
+		},
+		UINT32Size: func(bytes []byte) int {
+			return int(binary.BigEndian.Uint32(bytes))
+		},
+		UINT64Size: func(bytes []byte) int {
+			return int(binary.BigEndian.Uint64(bytes))
+		},
+	}
+
+	numToBytesFuncMap = map[int]NumToBytesFuncType{
+		UINT8Size: func(num int, bytes []byte) {
+			bytes[0] = byte(num)
+		},
+		UINT16Size: func(num int, bytes []byte) {
+			binary.BigEndian.PutUint16(bytes, uint16(num))
+		},
+		UINT32Size: func(num int, bytes []byte) {
+			binary.BigEndian.PutUint32(bytes, uint32(num))
+		},
+		UINT64Size: func(num int, bytes []byte) {
+			binary.BigEndian.PutUint64(bytes, uint64(num))
+		},
+	}
+)
+
+func GetBytesToNumberFunc(numType int) BytesToNumFuncType {
+	return bytesToNumFuncMap[numType]
+}
+
+func GetNumberToBytesFunc(numType int) NumToBytesFuncType {
+	return numToBytesFuncMap[numType]
+}
